290964c5ae7a7473e12a5744d0e928e380c17938
adezz <1352483315@qq.com>
2020-06-03 20:25:37 +0800

    fina
    fina

--- backup/test0912.py
+++
@@ -1,9 +0,0 @@
-import socket
-
-ip = "192.168.43.10"
-s = socket.socket()
-s.connect((str(ip), 6379))
-s.send(b"INFO\r\n")
-result = s.recv(1024)
-if b"redis_version" in result:
-    print("redis a")
